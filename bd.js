78402ce1e04891912fe8ab2b5c151185e7c66d74
franco Mancilla <134561073+frankito45@users.noreply.github.com>
2025-11-02 17:44:15 -0300

    Add files via upload

---
+++ bd.js
@@ -0,0 +1,75 @@
+
+import { MongoClient, ServerApiVersion, ObjectId} from 'mongodb';
+
+const user = process.env.USER_BD
+const db_password = process.env.KEY_BD
+
+const url = `mongodb+srv://${user}:${db_password}@test.slygxwk.mongodb.net/?appName=test`;
+
+export class MongoDB {
+    constructor(url,dbName) {
+        this.client = new MongoClient(url,
+        {
+            serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,}
+        })
+
+        this.dbName = dbName;
+        this.db = null;
+        this.collection = null;
+    }
+
+    async connect(){
+        await this.client.connect();
+            // Send a ping to confirm a successful connection
+        await this.client.db("admin").command({ ping: 1 });
+        console.log('conectado a la bd')
+        // this.db = await awthis.client.db(this.dbName);
+    }
+
+    async getCollection(collectionName){
+        if (!this.db) {
+            this.db = this.client.db(this.dbName);
+            this.collection = this.db.collection(collectionName);   
+        }
+    }
+    async getAll(){
+        return await this.collection.find({}).toArray()
+    }
+    
+    
+    async close() {
+        await this.client.close()
+        console.log('conexion cerrada')
+    }
+    
+    
+    
+    async getAll(){
+        return await this.collection.find({}).toArray()
+    }
+    async getById(id,) {
+        return await this.collection.findOne({ _id: new ObjectId(id) });
+    }
+    
+    async insertItem(doc,) {
+        await this.collection.insertOne(doc);
+    }
+    
+    async update(id, patch,) {
+        return await this.collection.updateOne({ _id: new ObjectId(id) }, { $set: patch });
+    }
+    
+    async delete(id) {
+        return await this.collection.deleteOne({ _id: new ObjectId(id) });
+    }
+    
+}
+
+
+
+
+export default  MongoDB
+
