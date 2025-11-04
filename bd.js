804a41c6f6ea29fff2b8cf59b5c71f2271678369
franco <mancillafranco71@gmail.com>
2025-11-03 22:10:17 -0300

    correcion de base de datos

--- bd.js
+++ bd.js
@@ -1,90 +1,71 @@
 
-// import { MongoClient, ServerApiVersion, ObjectId} from 'mongodb';
+import { MongoClient, ServerApiVersion, ObjectId} from 'mongodb';
 
-// const user = process.env.USER_BD
-// const db_password = process.env.KEY_BD
+export class MongoDB {
+    constructor(url,dbName) {
+        this.client = new MongoClient(url, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            },
 
-// // Configuración de la URL de conexión con parámetros SSL explícitos
-// const url = `mongodb+srv://${user}:${db_password}@test.slygxwk.mongodb.net/?ssl=true&retryWrites=true&w=majority`;
+        })
 
-// // Configuración SSL/TLS más permisiva para desarrollo
-// const isDevEnvironment = process.env.NODE_ENV !== 'production';
-// const ssl = true; // Siempre usar SSL
-// const sslValidate = !isDevEnvironment; // Solo validar certificados en producción
+        this.dbName = dbName;
+        this.db = null;
+        this.collection = null;
+    }
 
-// export class MongoDB {
-//     constructor(url,dbName) {
-//         this.client = new MongoClient(url, {
-//             serverApi: {
-//                 version: ServerApiVersion.v1,
-//                 strict: true,
-//                 deprecationErrors: true,
-//             },
-//             ssl: ssl, // Siempre usar SSL
-//         })
+    async connect(){
+        try {
+            await this.client.connect();
+            // Send a ping to confirm a successful connection
+            await this.client.db("admin").command({ ping: 1 });
+            console.log('Conexión exitosa a MongoDB');
+        } catch (error) {
+            console.error('Error de conexión a MongoDB:', error);
+            throw error;
+        }
+    }
 
-//         this.dbName = dbName;
-//         this.db = null;
-//         this.collection = null;
-//     }
-
-//     async connect(){
-//         try {
-//             await this.client.connect();
-//             // Send a ping to confirm a successful connection
-//             await this.client.db("admin").command({ ping: 1 });
-//             console.log('Conexión exitosa a MongoDB');
-//         } catch (error) {
-//             console.error('Error de conexión a MongoDB:', error);
-//             if (error.message.includes('SSL routines')) {
-//                 console.error('Error SSL/TLS. Verifica la configuración SSL y los certificados.');
-//             }
-//             throw error;
-//         }
-//     }
-
-//     async getCollection(collectionName){
-//         if (!this.db) {
-//             this.db = this.client.db(this.dbName);
-//             this.collection = this.db.collection(collectionName);   
-//         }
-//     }
-//     async getAll(){
-//         return await this.collection.find({}).toArray()
-//     }
+    async getCollection(collectionName){
+        if (!this.db) {
+            this.db = this.client.db(this.dbName);
+            this.collection = this.db.collection(collectionName);   
+        }
+    }
+    async getAll(){
+        return await this.collection.find({}).toArray()
+    }
     
     
-//     async close() {
-//         await this.client.close()
-//         console.log('conexion cerrada')
-//     }
+    async close() {
+        await this.client.close()
+        console.log('conexion cerrada')
+    }
     
     
     
-//     async getAll(){
-//         return await this.collection.find({}).toArray()
-//     }
-//     async getById(id,) {
-//         return await this.collection.findOne({ _id: new ObjectId(id) });
-//     }
+    async getAll(){
+        return await this.collection.find({}).toArray()
+    }
+    async getById(id,) {
+        return await this.collection.findOne({ _id: new ObjectId(id) });
+    }
     
-//     async insertItem(doc,) {
-//         await this.collection.insertOne(doc);
-//     }
+    async insertItem(doc) {
+        await this.collection.insertOne(doc);
+    }
     
-//     async update(id, patch,) {
-//         return await this.collection.updateOne({ _id: new ObjectId(id) }, { $set: patch });
-//     }
+    async update(id, patch,) {
+        return await this.collection.updateOne({ _id: new ObjectId(id) }, { $set: patch });
+    }
     
-//     async delete(id) {
-//         return await this.collection.deleteOne({ _id: new ObjectId(id) });
-//     }
+    async delete(id) {
+        return await this.collection.deleteOne({ _id: new ObjectId(id) });
+    }
     
-// }
+}
 
 
-
-
-// export default  MongoDB
-
-// // lsita
--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 import express from "express";
 import fs from 'fs';
+import { MongoDB } from './bd.js';
+
+const user = process.env.USER_BD;
+const password = process.env.KEY_BD;
+
+const url = `mongodb+srv://${user}:${password}@test.slygxwk.mongodb.net/?ssl=true&retryWrites=true&w=majority`;
+// cargar base de datos 
+const database = new MongoDB(url,"sample_mflix")
+database.connect();
+database.getCollection("granjapico");
+
+
 
 // Cargar data.json sin usar import assertions (más compatible)
-const data = JSON.parse(fs.readFileSync(new URL('./data.json', import.meta.url), 'utf8'));
+// const data = JSON.parse(fs.readFileSync(new URL('./data.json', import.meta.url), 'utf8'));
 import cors from 'cors';
 
 
@@ -17,32 +29,30 @@ app.use(cors());
 
 app.get('/', async (req, res) => {
     // Return a simple API message
-    return res.status(200).json({ message: 'API is running' });
+    const dataFromDb =  await database.getAll();
+    console.log(dataFromDb);
+    return res.status(200).json(dataFromDb);
 });
 
+
+app.post('/stock/', async (req, res) => {
+    const newItem = req.body;
+    database.insertItem(newItem);
+    return res.status(201).json({ message: 'Item added', item: newItem });
+
+});
 
 // Example GET for a single stock item (placeholder)
 app.get('/stock/:id', async (req, res) => {
     const { id } = req.params;
-    for (const item of data.files) {
-        if (item.id === parseInt(id)) {
-            return res.status(200).json(item);
-        }else{
-            return res.status(404).json({ message: 'Item not found' });
-        }}});
+    const item = await database.getById(id);
+    if (item) {
+        return res.status(200).json(item);
+    } else {
+        return res.status(404).json({ message: 'Item not found'})
+        }});
 
-app.get('/stock', async (req, res) => {
-    return res.status(200).json(data.files);
-});
 
-app.post('/stock/', async (req, res) => {
-    const newItem = req.body;
-    const id = data.files.length + 1;
-    newItem.id = id;    
-    data.files.push(newItem);
-    fs.writeFileSync(new URL('./data.json', import.meta.url), JSON.stringify(data, null, 2));
-    return res.status(201).json({ message: 'Item added', item: newItem });
-});
 
 app.listen(port, () => {
     console.log(`port http://localhost:${port}`);
